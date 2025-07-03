d130b90fcb51327d541b6177197abf8a42a835c4
ayushadid <ayush.adid@gmail.com>
2025-07-03 19:17:04 +0530

    path

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 import express from 'express';
 import { MongoClient } from 'mongodb';
 const app = express();
